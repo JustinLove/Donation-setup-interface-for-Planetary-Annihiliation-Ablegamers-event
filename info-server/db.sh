4bddf0267933e4d21973733e7aad3ae0afcf11a3
Justin Love <git@JustinLove.name>
2016-07-20 19:22:48 -0500

    game database

---
+++ info-server/db.sh
@@ -0,0 +1,1 @@
+redis-server db/dev.conf
--- info-server/index.js
+++ info-server/index.js
@@ -1,7 +1,13 @@
+var Redis = require('redis')
+var redis = Redis.createClient()
+redis.on('error', function(err) {
+  console.log('Redis error', err)
+})
+
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var path = require('path')
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -10,9 +16,26 @@ app.use(function(req, res, next) {
 });
 
 app.get('/options.json', function(req, res){
-  res.sendFile(__dirname + '/options.json');
+  redis.get('game', function(err, reply) {
+    if (reply) {
+      res.json(JSON.parse(reply))
+    } else {
+      res.sendStatus(404)
+    }
+  })
 });
 
+app.put('/options.json', jsonParser, function(req, res){
+  redis.set('game', JSON.stringify(req.body), function(err, reply) {
+    if (reply == 'OK') {
+      res.sendStatus(200)
+    } else {
+      res.sendStatus(500)
+    }
+  })
+});
+
+var http = require('http').Server(app);
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });

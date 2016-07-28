f836ce659aca2540cd65985c87d7b19e7003ec7e
Justin Love <git@JustinLove.name>
2016-07-28 18:51:17 -0500

    CORS delete method

--- info-server/index.js
+++ info-server/index.js
@@ -39,6 +39,7 @@ var jsonParser = bodyParser.json()
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST, OPTIONS");
   next();
 });
 

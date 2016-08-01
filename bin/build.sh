35709199263269b384ea3250adf5459bcaec2935
Justin Love <git@JustinLove.name>
2016-08-01 15:56:18 -0500

    updatea r.js path

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,2 @@
 cp -R public/* dist/
-node r.js -o baseUrl=public name=almond include=donate-main out=dist/require.js paths.requireLib=require
+node bin/r.js -o baseUrl=public name=almond include=donate-main out=dist/require.js paths.requireLib=require

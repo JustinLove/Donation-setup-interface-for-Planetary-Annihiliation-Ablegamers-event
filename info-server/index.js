6ea446faae926c42236939c94830eb835084fd2b
Justin Love <git@JustinLove.name>
2017-09-05 10:04:22 -0500

    add retry functoin to try and fix ECONNRESET errors

--- info-server/index.js
+++ info-server/index.js
@@ -1,5 +1,26 @@
 var Redis = require('redis')
-var redis = Redis.createClient(process.env.REDIS_URL)
+var redis = Redis.createClient({
+  url: process.env.REDIS_URL,
+  // source: https://www.npmjs.com/package/redis
+  retry_strategy: function (options) {
+    if (options.error && options.error.code === 'ECONNREFUSED') {
+      // End reconnecting on a specific error and flush all commands with 
+      // a individual error 
+      return new Error('The server refused the connection');
+    }
+    if (options.total_retry_time > 1000 * 60 * 60) {
+      // End reconnecting after a specific timeout and flush all commands 
+      // with a individual error 
+      return new Error('Retry time exhausted');
+    }
+    if (options.attempt > 10) {
+      // End reconnecting with built in error 
+      return undefined;
+    }
+    // reconnect after 
+    return Math.min(options.attempt * 100, 3000);
+  }
+})
 redis.on('error', function(err) {
   console.log('Redis error', err)
 })

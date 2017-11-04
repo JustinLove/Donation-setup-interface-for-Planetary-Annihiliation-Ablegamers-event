e1a4658db6e2fb220007d36ba0e7f34f95a055b2
Justin Love <git@JustinLove.name>
2017-11-04 11:03:56 -0500

    set public and cache on upload

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,1 +1,2 @@
-s3cmd sync dist/ s3://wondible-com-ablegamers-2016 
+s3cmd sync --acl-public --add-header="Cache-Control:max-age=600" dist/ s3://wondible-com-ablegamers-2017 
+s3cmd modify --add-header="Cache-Control:max-age=86400" s3://wondible-com-ablegamers-2017/pa

6b151130074cccc743353b9178d0e9f935bd9c69
Justin Love <git@JustinLove.name>
2017-11-04 11:26:50 -0500

    wss for secure sockets

--- src/Config.elm
+++ src/Config.elm
@@ -4,5 +4,5 @@ config =
   { server = "http://localhost:5000/"
   --{ server = "https://ablegamers2017.herokuapp.com/"
   , wsserver = "ws://localhost:5000/"
-  --, wsserver = "ws://ablegamers2017.herokuapp.com"
+  --, wsserver = "wss://ablegamers2017.herokuapp.com/"
   }

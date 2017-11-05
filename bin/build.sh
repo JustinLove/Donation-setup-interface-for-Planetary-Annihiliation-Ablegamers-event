b93833dcc91b8dd870a29eecc7f05735014955a1
Justin Love <git@JustinLove.name>
2017-11-05 14:54:28 -0600

    use a different requirejs for donate so it can be optimized with impacting other pages

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,2 @@
-cp -R public/* dist/
-node bin/r.js -o baseUrl=public name=almond include=donate-main out=dist/require.js paths.requireLib=require
+cp -R public/* dist
+node bin/r.js -o baseUrl=public name=almond include=donate-main out=dist/require-donate.js paths.requireLib=require

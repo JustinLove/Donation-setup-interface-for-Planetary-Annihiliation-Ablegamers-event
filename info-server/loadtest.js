e6d7c46657854472a035a5a20ed9c28cb2f4eb67
Justin Love <git@JustinLove.name>
2017-11-05 07:04:39 -0600

    blank games for this year

---
+++ info-server/loadtest.js
@@ -0,0 +1,35 @@
+var fs = require('fs')
+var data = fs.readFileSync(__dirname+'/options-2016.json')
+var json = JSON.parse(data)
+
+var Redis = require('redis')
+var redis = Redis.createClient(process.env.REDIS_URL)
+redis.on('error', function(err) {
+  console.log('Redis error', err)
+})
+
+var count = json.games.length
+var loadGames = function() {
+  json.games.forEach(function(game) {
+    redis.set(game.id, JSON.stringify(game), function(err, ok) {
+      Redis.print(err, ok)
+      if (--count == 0) {
+        loadList()
+      }
+    })
+  })
+}
+
+var games = json.games.map(function(game) {
+  return game.id
+})
+var loadList = function() {
+  console.log(games)
+  redis.sadd('games', games, function(err, ok) {
+    Redis.print(err, ok)
+    process.exit()
+  })
+}
+
+
+loadGames()

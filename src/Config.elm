6508c022e9aefc94c5d92229d0b46499465738ea
Justin Love <git@JustinLove.name>
2017-11-27 13:02:35 -0600

    2017 database donation dump

--- src/Config.elm
+++ src/Config.elm
@@ -1,8 +1,8 @@
 module Config exposing (config)
 
 config =
-  { server = "http://localhost:5000/"
-  --{ server = "https://ablegamers2017.herokuapp.com/"
-  , wsserver = "ws://localhost:5000/"
-  --, wsserver = "wss://ablegamers2017.herokuapp.com/"
+  --{ server = "http://localhost:5000/"
+  { server = "https://ablegamers2017.herokuapp.com/"
+  --, wsserver = "ws://localhost:5000/"
+  , wsserver = "wss://ablegamers2017.herokuapp.com/"
   }
